8bc4d7e3887b3f27c10a0ad0cfe2a1a0050570de
vmk1vmk <vmk1vmk@googlemail.com>
2019-05-10 14:23:37 +0200

    Removed unnecessary modules includeing advertisement, ads core and xml.

--- src/videoplayer.ts
+++ src/videoplayer.ts
@@ -1,54 +1,59 @@
-import { Player, PlayerAPI } from 'bitmovin-player/modules/bitmovinplayer-core';
-import { PlayerConfig, SourceConfig } from 'bitmovin-player/types/core/PlayerConfigAPI';
-import AbrModule from 'bitmovin-player/modules/bitmovinplayer-abr';
-import Mp4Module from 'bitmovin-player/modules/bitmovinplayer-container-mp4';
-import ContainerTsModule from 'bitmovin-player/modules/bitmovinplayer-container-ts';
-import EngineBitmovin from 'bitmovin-player/modules/bitmovinplayer-engine-bitmovin';
-import EngineNative from 'bitmovin-player/modules/bitmovinplayer-engine-native';
-import HlsModule from 'bitmovin-player/modules/bitmovinplayer-hls';
-import MseRendererModule from 'bitmovin-player/modules/bitmovinplayer-mserenderer';
-import StyleModule from 'bitmovin-player/modules/bitmovinplayer-style';
-import AdvertisingCoreModule from 'bitmovin-player/modules/bitmovinplayer-advertising-core';
-import AdvertisingModule from 'bitmovin-player/modules/bitmovinplayer-advertising-ima';
-import XMLModule from 'bitmovin-player/modules/bitmovinplayer-xml';
+import { Player, PlayerAPI } from "bitmovin-player/modules/bitmovinplayer-core";
+import {
+  PlayerConfig,
+  SourceConfig
+} from "bitmovin-player/types/core/PlayerConfigAPI";
+import AbrModule from "bitmovin-player/modules/bitmovinplayer-abr";
+import Mp4Module from "bitmovin-player/modules/bitmovinplayer-container-mp4";
+import ContainerTsModule from "bitmovin-player/modules/bitmovinplayer-container-ts";
+import EngineBitmovin from "bitmovin-player/modules/bitmovinplayer-engine-bitmovin";
+import EngineNative from "bitmovin-player/modules/bitmovinplayer-engine-native";
+import MseRendererModule from "bitmovin-player/modules/bitmovinplayer-mserenderer";
+import StyleModule from "bitmovin-player/modules/bitmovinplayer-style";
 
-import { UIFactory } from './ui-factory';
+import { UIFactory } from "./ui-factory";
 
-const videoElementId: string = 'demo-player';
-const domElement: HTMLElement | null = document.getElementById(videoElementId);
+const VIDEO_ELEMENT_ID: string = "demo-player";
 
-if (!domElement) {
-    throw new Error(`No Element with id ${videoElementId} was found.`);
-}
+(function playerSetup(): void {
+  const domElement: HTMLElement | null = document.getElementById(
+    VIDEO_ELEMENT_ID
+  );
 
-const playerConfig: PlayerConfig = {
-    key: process.env.BITMOVIN_API_KEY || ''
-};
+  if (!domElement) {
+    throw new Error(`No Element with id ${VIDEO_ELEMENT_ID} was found.`);
+  }
 
-const sourceConfig: SourceConfig = {
-    title: 'Invalid Source example',
-    description: 'An invalid source is loaded and the ui structure is duplicated',
-    progressive: '/non-existent-video.mp4'
-};
+  const playerConfig: PlayerConfig = {
+    key: process.env.BITMOVIN_API_KEY || "",
+    playback: {
+      muted: true
+    }
+  };
 
-[
+  const sourceConfig: SourceConfig = {
+    title: "Invalid Source example",
+    progressive:
+      "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/MI201109210084_mpeg-4_hd_high_1080p25_10mbits.mp4" // source: https://bitmovin.com/demos/stream-test
+  };
+
+  [
     EngineBitmovin,
     EngineNative,
     MseRendererModule,
-    HlsModule,
     AbrModule,
     Mp4Module,
     ContainerTsModule,
-    StyleModule,
-    XMLModule,
-    AdvertisingCoreModule,
-    AdvertisingModule
-].forEach(Player.addModule);
+    StyleModule
+  ].forEach(Player.addModule);
 
-const videoPlayer: PlayerAPI = new Player(domElement, playerConfig);
+  const videoPlayer: PlayerAPI = new Player(domElement, playerConfig);
 
-UIFactory.buildUI(videoPlayer, playerConfig);
+  UIFactory.buildUI(videoPlayer, playerConfig);
 
-videoPlayer.load(sourceConfig).catch(
-    (error: Error) => console.log(`Error during source loading -> ${error.message}`)
-);
+  videoPlayer
+    .load(sourceConfig)
+    .catch((error: Error) =>
+      console.log(`Error during source loading -> ${error.message}`)
+    );
+})();
